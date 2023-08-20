cf841b319868f50b1a7942414c9f07b5973df281
chribro88 <chrisbrown@live.com.au>
2023-08-20 18:05:35 +1000

    Update start-kafka.sh

--- start-kafka.sh
+++ start-kafka.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/kafka/bin/kafka-storage.sh format --config /kafka/config/server.properties --cluster-id 'EP6hyiddQNW5FPrAvR9kWw' --ignore-formatted
+/kafka/bin/kafka-storage.sh format --config /kafka/config/server.properties --cluster-id "$KAFKA_CLUSTER_ID" --ignore-formatted
 
 /kafka/bin/kafka-server-start.sh /kafka/config/server.properties
