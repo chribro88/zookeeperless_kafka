ae6d39b0269234f866db69dcd978382359bfe96f
chribro88 <chrisbrown@live.com.au>
2023-08-20 21:19:32 +1000

    Update start-kafka.sh

--- start-kafka.sh
+++ start-kafka.sh
@@ -2,4 +2,4 @@
 
 /kafka/bin/kafka-storage.sh format --config /kafka/config/server.properties --cluster-id "$KAFKA_CLUSTER_ID" --ignore-formatted
 
-/kafka/bin/kafka-server-start.sh /kafka/config/server.properties --override node.id="$KAFKA_NODE_ID" --override advertised.listeners=PLAINTEXT://$KAFKA_NODE_NAME:9192,LISTENER_DOCKER_EXTERNAL://$KAFKA_NODE_IP:9092
+/kafka/bin/kafka-server-start.sh /kafka/config/server.properties --override node.id=$KAFKA_NODE_ID --override advertised.listeners=PLAINTEXT://$KAFKA_NODE_NAME:9192,LISTENER_DOCKER_EXTERNAL://$KAFKA_NODE_IP:9092
