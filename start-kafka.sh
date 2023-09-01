80fa0fc2b2f9e627b73189daf5c9c6b6a85ed069
chribro88 <chrisbrown@live.com.au>
2023-09-01 21:26:36 +1000

    Update start-kafka.sh

--- start-kafka.sh
+++ start-kafka.sh
@@ -7,6 +7,7 @@ cat /kafka/config/server.properties.template | sed \
   -e "s|{{KAFKA_QUORUM_VOTERS}}|${KAFKA_QUORUM_VOTERS:-1@kafka1:9093,2@kafka2:9093,3@kafka3:9093}|g" \
   -e "s|{{KAFKA_LISTENERS}}|${KAFKA_LISTENERS:-PLAINTEXT://$HOSTNAME:9192,CONTROLLER://$HOSTNAME:9093,LISTENER_DOCKER_EXTERNAL://0.0.0.0:9092}|g" \
   -e "s|{{KAFKA_ADVERTISED_LISTENERS}}|${KAFKA_ADVERTISED_LISTENERS:-PLAINTEXT://$HOSTNAME:9192,LISTENER_DOCKER_EXTERNAL://$IP:9092}|g" \
+  -e "s|{{KAFKA_AUTO_TOPIC_CREATION_ENABLE}}|${KAFKA_AUTO_TOPIC_CREATION_ENABLE:-false}|g" \
    > /kafka/config/server.properties
    
 /kafka/bin/kafka-storage.sh format --config /kafka/config/server.properties --cluster-id "$KAFKA_CLUSTER_ID" --ignore-formatted
